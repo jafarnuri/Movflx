a845c8f862bb136b5eecb1d565b4e9cb3a36269b
jafarnuri <jafarnuri1994@gmail.com>
2025-01-04 19:21:53 +0400

    "Continuation of the admin section"

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,21 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    use HasFactory;
-
-    protected $fillable = [
-        'title',
-        'slug',
-        'image',
-        'content',
-        'author',
-        'likes',
-        'comments_count',
-        'published_at',
-    ];
+    //
 }
